0020576458c811862c003e4ed2c3b651d117ce54
OS <hoquanghuy0123@gmail.com>
2021-09-10 21:35:37 +0700

    code register #1

---
+++ src/main/java/com/hqyuh/springredditclone/controller/AuthController.java
@@ -0,0 +1,16 @@
+package com.hqyuh.springredditclone.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @PostMapping("/signup")
+    public void signup(){
+
+    }
+
+}
