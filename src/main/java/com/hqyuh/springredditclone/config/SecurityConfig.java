229cc910fd8157c7a3ecba02e3a112edfed70b62
OS <hoquanghuy0123@gmail.com>
2021-09-12 20:59:55 +0700

    code token-based authentication #2

--- src/main/java/com/hqyuh/springredditclone/config/SecurityConfig.java
+++ src/main/java/com/hqyuh/springredditclone/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.hqyuh.springredditclone.config;
 
+import com.hqyuh.springredditclone.security.JwtAuthenticationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -12,12 +13,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
 
     @Bean(BeanIds.AUTHENTICATION_MANAGER)
@@ -40,6 +43,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .anyRequest()
                 .authenticated();
+
+        // thêm lớp Filter dề kiểm tra jwt
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
     }
 
     @Autowired
